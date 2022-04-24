f9ca3ea40da7eedc66b29b481c5f6bd5ebade0ce
Vic Ong <vic_ong@thecdm.ca>
2022-04-24 10:00:04 -0700

    Put entry point in a main function

--- src/encanto.js
+++ src/encanto.js
@@ -9,115 +9,119 @@ const TIMESTEP = 1 / 30;
 const ITERATIONS = 30;
 const DAMPING_COEFFICIENT = 0.99;
 
-// Initializations
-initRender({
-  camera: {
-    center: [14, 5, 0],
-    distance: 30,
-    theta: degToRad(92),
-    phi: degToRad(5),
-  }
-});
+const main = () => {
+  // Initializations
+  initRender({
+    camera: {
+      center: [14, 5, 0],
+      distance: 30,
+      theta: degToRad(92),
+      phi: degToRad(5),
+    }
+  });
 
-// Get meshes
-const axes = createAxes();
-const roofTopMesh = createEncantoRoofMesh({
-  subdivisions: 200,
-  length: 30,
-  mass: 1,
-  offset: [0, 10, 0],
-  color: getColor(234, 111, 81),
-  timestep: TIMESTEP,
-  gapLength: 15,
-  force: [0, 5, 0],
-  reverse: false,
-});
-const roofLeftCenterMesh = createEncantoRoofMesh({
-  subdivisions: 150,
-  length: 10,
-  mass: 1,
-  offset: [0, 4, 3],
-  color: getColor(181, 67, 38),
-  timestep: TIMESTEP,
-  gapLength: 20,
-  force: [0, 7, 0],
-  reverse: true,
-  time: {
-    delay: 1.5,
-    postDelay: 1.5,
-  },
-});
-const roofFrontCenterMesh = createEncantoRoofMesh({
-  subdivisions: 150,
-  length: 10,
-  mass: 1,
-  offset: [10, 4, 6],
-  color: getColor(149, 12, 58),
-  timestep: TIMESTEP,
-  gapLength: 20,
-  force: [0, 7, 0],
-  reverse: true,
-  time: {
-    delay: 0.75,
-    postDelay: 1.5,
-  },
-});
-const roofRightCenterMesh = createEncantoRoofMesh({
-  subdivisions: 150,
-  length: 10,
-  mass: 1,
-  offset: [20, 4, 3],
-  color: getColor(198, 90, 147),
-  timestep: TIMESTEP,
-  gapLength: 20,
-  force: [0, 7, 0],
-  reverse: true,
-  time: {
-    delay: 0,
-    postDelay: 1.5,
-  },
-});
+  // Get meshes
+  const axes = createAxes();
+  const roofTopMesh = createEncantoRoofMesh({
+    subdivisions: 200,
+    length: 30,
+    mass: 1,
+    offset: [0, 10, 0],
+    color: getColor(234, 111, 81),
+    timestep: TIMESTEP,
+    gapLength: 15,
+    force: [0, 5, 0],
+    reverse: false,
+  });
+  const roofLeftCenterMesh = createEncantoRoofMesh({
+    subdivisions: 150,
+    length: 10,
+    mass: 1,
+    offset: [0, 4, 3],
+    color: getColor(181, 67, 38),
+    timestep: TIMESTEP,
+    gapLength: 20,
+    force: [0, 7, 0],
+    reverse: true,
+    time: {
+      delay: 1.5,
+      postDelay: 1.5,
+    },
+  });
+  const roofFrontCenterMesh = createEncantoRoofMesh({
+    subdivisions: 150,
+    length: 10,
+    mass: 1,
+    offset: [10, 4, 6],
+    color: getColor(149, 12, 58),
+    timestep: TIMESTEP,
+    gapLength: 20,
+    force: [0, 7, 0],
+    reverse: true,
+    time: {
+      delay: 0.75,
+      postDelay: 1.5,
+    },
+  });
+  const roofRightCenterMesh = createEncantoRoofMesh({
+    subdivisions: 150,
+    length: 10,
+    mass: 1,
+    offset: [20, 4, 3],
+    color: getColor(198, 90, 147),
+    timestep: TIMESTEP,
+    gapLength: 20,
+    force: [0, 7, 0],
+    reverse: true,
+    time: {
+      delay: 0,
+      postDelay: 1.5,
+    },
+  });
 
-// Get solver
-const solver = () => {
-  solve({
-    mesh: roofTopMesh,
-    damping: DAMPING_COEFFICIENT,
-    timestep: TIMESTEP,
-    iterationCount: ITERATIONS,
-  });
-  solve({
-    mesh: roofRightCenterMesh,
-    damping: DAMPING_COEFFICIENT,
-    timestep: TIMESTEP,
-    iterationCount: ITERATIONS,
-  });
-  solve({
-    mesh: roofFrontCenterMesh,
-    damping: DAMPING_COEFFICIENT,
-    timestep: TIMESTEP,
-    iterationCount: ITERATIONS,
-  });
-  solve({
-    mesh: roofLeftCenterMesh,
-    damping: DAMPING_COEFFICIENT,
-    timestep: TIMESTEP,
-    iterationCount: ITERATIONS,
-  });
+  // Get solver
+  const solver = () => {
+    solve({
+      mesh: roofTopMesh,
+      damping: DAMPING_COEFFICIENT,
+      timestep: TIMESTEP,
+      iterationCount: ITERATIONS,
+    });
+    solve({
+      mesh: roofRightCenterMesh,
+      damping: DAMPING_COEFFICIENT,
+      timestep: TIMESTEP,
+      iterationCount: ITERATIONS,
+    });
+    solve({
+      mesh: roofFrontCenterMesh,
+      damping: DAMPING_COEFFICIENT,
+      timestep: TIMESTEP,
+      iterationCount: ITERATIONS,
+    });
+    solve({
+      mesh: roofLeftCenterMesh,
+      damping: DAMPING_COEFFICIENT,
+      timestep: TIMESTEP,
+      iterationCount: ITERATIONS,
+    });
+  };
+
+  // Render
+  const drawer = () => {
+    axes.forEach((axis) => draw(axis));
+    draw(roofTopMesh);
+    draw(getRopeLinesMesh(roofTopMesh));
+    draw(roofLeftCenterMesh);
+    draw(getRopeLinesMesh(roofLeftCenterMesh));
+    draw(roofRightCenterMesh);
+    draw(getRopeLinesMesh(roofRightCenterMesh));
+    draw(roofFrontCenterMesh);
+    draw(getRopeLinesMesh(roofFrontCenterMesh));
+  };
+
+  // Run program
+  updateFrame(solver, drawer);
 };
 
-// Render
-const drawer = () => {
-  axes.forEach((axis) => draw(axis));
-  draw(roofTopMesh);
-  draw(getRopeLinesMesh(roofTopMesh));
-  draw(roofLeftCenterMesh);
-  draw(getRopeLinesMesh(roofLeftCenterMesh));
-  draw(roofRightCenterMesh);
-  draw(getRopeLinesMesh(roofRightCenterMesh));
-  draw(roofFrontCenterMesh);
-  draw(getRopeLinesMesh(roofFrontCenterMesh));
-};
-
-// Run program
-updateFrame(solver, drawer);
+main();
--- src/inflatableAirPuppet.js
+++ src/inflatableAirPuppet.js
@@ -10,63 +10,65 @@ const TIMESTEP = 1 / 60;
 const ITERATIONS = 30;
 const DAMPING_COEFFICIENT = 0.99;
 const GRAVITY = [0, -10, 0];
-
-// Adjustable parameters
 const PRESSURE_STRENGTH = 1;
 const WIND_STRENGTH = 1;
 
-// Initializations
-initRender({
-  camera: {
-    center: [3, 5, 0],
-    distance: 25,
-    theta: degToRad(80),
-    phi: degToRad(10),
-  }
-});
-
-// Get meshes
-const axes = createAxes();
-const ropeMesh = createRopeMesh({
-  subdivisions: 100,
-  length: 15,
-  mass: 1,
-  offset: [0, 0, 0],
-  color: getColor(213, 197, 47),
-});
-ropeMesh.attrs.constraints.positions = [
-  {
-    i: 0,
-    position: ropeMesh.vertices[0].position,
-  },
-];
-
-// Get solver
-const solver = () => {
-  const pressure = [0, getRandomInt(10, 30), 0];
-  const wind = [getRandomInt(-20, 20), 0, getRandomInt(-20, 20)];
-  scale(pressure, pressure, PRESSURE_STRENGTH);
-  scale(wind, wind, WIND_STRENGTH);
-
-  solve({
-    mesh: ropeMesh,
-    damping: DAMPING_COEFFICIENT,
-    timestep: TIMESTEP,
-    iterationCount: ITERATIONS,
-    props: {
-      gravity: GRAVITY,
-      pressure,
-      wind,
-    },
+const main = () => {
+  // Initializations
+  initRender({
+    camera: {
+      center: [3, 5, 0],
+      distance: 25,
+      theta: degToRad(80),
+      phi: degToRad(10),
+    }
   });
+
+  // Get meshes
+  const axes = createAxes();
+  const ropeMesh = createRopeMesh({
+    subdivisions: 100,
+    length: 15,
+    mass: 1,
+    offset: [0, 0, 0],
+    color: getColor(213, 197, 47),
+  });
+  ropeMesh.attrs.constraints.positions = [
+    {
+      i: 0,
+      position: ropeMesh.vertices[0].position,
+    },
+  ];
+
+  // Get solver
+  const solver = () => {
+    const pressure = [0, getRandomInt(10, 30), 0];
+    const wind = [getRandomInt(-20, 20), 0, getRandomInt(-20, 20)];
+    scale(pressure, pressure, PRESSURE_STRENGTH);
+    scale(wind, wind, WIND_STRENGTH);
+
+    solve({
+      mesh: ropeMesh,
+      damping: DAMPING_COEFFICIENT,
+      timestep: TIMESTEP,
+      iterationCount: ITERATIONS,
+      props: {
+        gravity: GRAVITY,
+        pressure,
+        wind,
+      },
+    });
+  };
+
+  // Render
+  const drawer = () => {
+    axes.forEach((axis) => draw(axis));
+    draw(ropeMesh);
+    draw(getRopeLinesMesh(ropeMesh));
+  };
+
+  // Run program
+  updateFrame(solver, drawer);
 };
 
-// Render
-const drawer = () => {
-  axes.forEach((axis) => draw(axis));
-  draw(ropeMesh);
-  draw(getRopeLinesMesh(ropeMesh));
-};
-
-// Run program
-updateFrame(solver, drawer);
+main();
